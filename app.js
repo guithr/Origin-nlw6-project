0770b7eec67cc20059565cf602e14fe072bece10
Guilherme Matos <guilherme.thrmatos@gmail.com>
2024-02-21 00:24:05 -0300

    Finalized project and refactored code for improved readability and maintainability.

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
-const links = document.querySelectorAll("nav ul li a");
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
 
-for (const element of toggle) {
-  element.addEventListener("click", function () {
+toggle.forEach((element) => {
+  element.addEventListener("click", () => {
     nav.classList.toggle("show");
   });
-}
+});
 
-for (const link of links) {
-  link.addEventListener("click", function () {
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll("nav ul li a");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
     nav.classList.remove("show");
   });
-}
+});
 
-window.addEventListener("scroll", function () {
+/* mudar o header da página quando der scroll */
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+const changeHeaderWhenScroll = () => {
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
   }
-});
+};
 
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   pagination: {
@@ -31,6 +35,12 @@ const swiper = new Swiper(".swiper", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 const scrollReveal = ScrollReveal({
@@ -41,11 +51,54 @@ const scrollReveal = ScrollReveal({
 });
 
 scrollReveal.reveal(
-  `#home .image, #home .text,
-  #about .image, #about text,
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
-  `,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+`,
   { interval: 100 }
 );
+/* Button back to top */
+const backToTopButton = document.querySelector(".back-to-top");
+const backToTop = () => {
+  if (window.scrollY >= 800) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+};
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll("main section[id]");
+const activeMenuAtCurrentSection = () => {
+  const checkpoint = window.scrollY + (window.innerHeight / 8) * 4;
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    const menuItem = document.querySelector(
+      `nav ul li a[href*="${sectionId}"]`
+    );
+    if (menuItem) {
+      if (checkpointStart && checkpointEnd) {
+        menuItem.classList.add("active");
+      } else {
+        menuItem.classList.remove("active");
+      }
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  changeHeaderWhenScroll();
+  backToTop();
+  activeMenuAtCurrentSection();
+});
